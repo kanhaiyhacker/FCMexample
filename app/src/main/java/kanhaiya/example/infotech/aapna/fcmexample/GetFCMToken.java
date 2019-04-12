7f609f08e7a54f7a9b7d45f40d58b0a802faef10
Kanhaiya Adhikari <kadhikari@aapnainfotech.com>
2019-04-12 14:13:36 +0530

    Initial commit

---
+++ app/src/main/java/kanhaiya/example/infotech/aapna/fcmexample/GetFCMToken.java
@@ -0,0 +1,17 @@
+package kanhaiya.example.infotech.aapna.fcmexample;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.io.IOException;
+
+public class GetFCMToken extends FirebaseInstanceIdService {
+    private static final String TAG="Testing";
+
+}
